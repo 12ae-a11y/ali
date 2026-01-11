2c8b40a96fa5f2f32c46aa78bdf370e143286e43
12ae-a11y <12alimhde@gmail.com>
2026-01-11 08:50:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,23 +2,26 @@
 // === إعدادات المتجر (قم بتعديل البيانات هنا لكل زبون) ===
 
 window.MY_STORE_CONFIG = {
-  firebase: {
-    apiKey: "AIzaSyBm5z1q56G7PUZH5qtuGpIx6ReWuzmaGAA",
-    authDomain: "jljl1111.firebaseapp.com",
-    databaseURL: "https://jljl1111-default-rtdb.firebaseio.com",
-    projectId: "jljl1111",
-    storageBucket: "jljl1111.firebasestorage.app",
-    messagingSenderId: "357923390988",
-    appId: "1:357923390988:web:bf4be9b0df5516ea1a5029",
-    measurementId: "G-MRVKVTNGJH"
-  },
+    // 1. ضع كود فيربايس هنا (نسخ ولصق من موقع فيربايس)
+    firebase: {
+        apiKey: "AIzaSyBm5z1q56G7PUZH5qtuGpIx6ReWuzmaGAA",
+        authDomain: "jljl1111.firebaseapp.com",
+        databaseURL: "https://jljl1111-default-rtdb.firebaseio.com",
+        projectId: "jljl1111",
+        storageBucket: "jljl1111.firebasestorage.app",
+        messagingSenderId: "357923390988",
+        appId: "1:357923390988:web:bf4be9b0df5516ea1a5029",
+        measurementId: "G-MRVKVTNGJH"
+    },
 
-  cloudinary: {
-    cloudName: "doxjvpehx",
-    uploadPreset: "ghdhbc"
-  },
+    // 2. ضع إعدادات الصور (Cloudinary) هنا
+    cloudinary: {
+        cloudName: "doxjvpehx",  // اسم الكلاود
+        uploadPreset: "ghdhbc" // اسم البريسيت (تأكد من إنشائه في حساب الزبون)
+    },
 
-  security: {
-    adminCode: "1001"
-  }
+    // 3. رمز دخول الأدمن
+    security: {
+        adminCode: "1001"
+    }
 };
